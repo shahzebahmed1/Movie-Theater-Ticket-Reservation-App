0a365a72ff934afb1e6880bfe58ea0dfc2a44923
ryan-razi <ryan.razi@outlook.com>
2024-12-01 13:57:29 -0700

    created giftcard

--- GiftCard.java
+++ GiftCard.java
@@ -1,4 +1,31 @@
 
 public class GiftCard {
+	
+	private int giftCardID;
+	private double giftCardBalance;
+	private String expireDate;	
+	
+	
+	public GiftCard() {
+	}
+	
+	public int getGiftCardID() {
+		return giftCardID;
+	}
+	public void setGiftCardID(int giftCardID) {
+		this.giftCardID = giftCardID;
+	}
+	public double getGiftCardBalance() {
+		return giftCardBalance;
+	}
+	public void setGiftCardBalance(double giftCardBalance) {
+		this.giftCardBalance = giftCardBalance;
+	}
+	public String getExpireDate() {
+		return expireDate;
+	}
+	public void setExpireDate(String expireDate) {
+		this.expireDate = expireDate;
+	}
 
 }
